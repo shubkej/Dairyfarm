51caa9702fe379ccbde26526b5a3eb34d5ec621b
shubham Kejriwal <shubhamkejriwal34@gmail.com>
2024-07-26 10:49:00 +0530

    name change in productitem

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -13,7 +13,7 @@ const ProductItem = ({id,name,price,description,image}) => {
                 <img className='product-item-image' src={image} alt="" />
                 {!cartItems[id] ?
                     <img className='add' src={assets.add_icon_white} onClick={() =>addTocart(id)} alt="" />
-                    : <div className='food-item-counter'>
+                    : <div className='product-item-counter'>
                         <img onClick={() => removeCartItems(id)} src={assets.remove_icon_red} alt="" />
                         <p>{ cartItems[id]}</p>
                         <img onClick={() => addTocart (id)} src={assets.add_icon_green} alt="" />
@@ -22,12 +22,12 @@ const ProductItem = ({id,name,price,description,image}) => {
             </div>
 
             <div className="product-item-info">
-                <div className="food-item-name-rating">
+                <div className="product-item-name-rating">
                     <p>{name}</p>
                     <img src={assets.rating_starts} alt="" />
                 </div>
-                <p className="food-item-desc">{ description}</p>
-                <p className="food-item-price">₹{ price}</p>
+                <p className="product-item-desc">{ description}</p>
+                <p className="product-item-price">₹{ price}</p>
             </div>
             
         </div>
